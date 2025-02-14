0269ede60a7e968003a26bddb630af2b21b48371
Полина <162202919+elb00406@users.noreply.github.com>
2025-02-14 10:42:24 +0300

    fix(texts)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,17 @@
-import styles from './Home.module.scss';
+import styles from "./Home.module.scss";
 
-import { Layout } from '../../components/layout/Layout';
-import Button from '../../components/ui/button/Button';
-import { Link } from 'react-router-dom';
-import { Excursion } from '../../components/layout/home/excursion/Excursion';
-import { Events } from '../../components/layout/home/events/Events';
-import { Intro } from '../../components/layout/intro/Intro';
+import { Layout } from "../../components/layout/Layout";
+import Button from "../../components/ui/button/Button";
+import { Link } from "react-router-dom";
+import { Excursion } from "../../components/layout/home/excursion/Excursion";
+import { Events } from "../../components/layout/home/events/Events";
+import { Intro } from "../../components/layout/intro/Intro";
 
 export const Home = () => {
     return (
         <div className={styles.home}>
             <Layout>
-                <Intro small={false} title={'Музей войны в Афганистане'} button />
+                <Intro small={false} title={"Музей войны в Афганистане"} button />
                 <section className={styles.about} id="about">
                     <div className={styles.inner}>
                         <div className={styles.text}>
@@ -19,13 +19,13 @@ export const Home = () => {
                                 Добро пожаловать <br />в наш Музей войны <br />в Афганистане
                             </h2>
                             <p>
-                                Музей войны в Афганистане школы № 13 г. Бреста принял своих первых
-                                посетителей в декабре 2006 года. Создание музея было инициировано
-                                Брестской городской организацией “Белорусский Союз ветеранов войны в
-                                Афганистане”. Основой для создания экспозиции послужили материалы,
-                                собранные учител математики Харкович Надеждой Владиславовной о
-                                выпускнике школы Ховане Валерии Ивановиче, героически погибшем в
-                                Афганистане в 1987 году...
+                                Музей войны в Афганистане средней школы № 13 г. Бреста принял своих
+                                первых посетителей в декабре 2006 года. Создание музея было
+                                инициировано Брестской городской организацией ОО “Белорусский союз
+                                ветеранов войны в Афганистане”. Основой для создания экспозиции
+                                послужили материалы, собранные учителем математики Харкович Надеждой
+                                Владиславовной о выпускнике школы Ховане Валерии Ивановиче,
+                                героически погибшем в Афганистане в 1987 году...
                             </p>
 
                             <Link to="about">
